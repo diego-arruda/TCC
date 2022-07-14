7710e29fd00a3e041eba46ff2259f245afff2401
Livia Leite de Almeida <lileal97@gmail.com>
2022-07-14 20:10:16 -0300

    Adicionando coluna de variacao do indice Ibovespa na tabela final

--- Matlab/Prog_Linear/main_functions.m
+++ Matlab/Prog_Linear/main_functions.m
@@ -3,8 +3,8 @@ clear all; close all; clc
 path_to_data = '../../python/dados_historicos_tratados/';
 
 % Train data
-initial_date = 'JAN19';
-final_date = 'DEZ19';
+initial_date = 'JAN20';
+final_date = 'DEZ20';
 
 index_file_name = strcat(path_to_data,'ibovespa_dados_historicos_tratado_', initial_date, '-', final_date, '.csv');
 fund_file_names = [
@@ -21,8 +21,8 @@ fund_file_names = [
 [ w4, z_otimo4 ] = linprog_functions( index_file_name, fund_file_names, 'DMinMax' );
 
 % Test data
-test_initial_date = 'JAN19';
-test_final_date = 'DEZ19';
+test_initial_date = 'JAN20';
+test_final_date = 'DEZ20';
 
 test_index_file_name = strcat(path_to_data,'ibovespa_dados_historicos_tratado_', test_initial_date, '-', test_final_date, '.csv');
 test_fund_file_names = [
@@ -77,8 +77,8 @@ set(gcf, 'PaperUnits', 'centimeters');
 set(gcf, 'PaperPosition', [0 0 30 15]);
 saveas(gca, fullfile(fname, fig_name));
 
-final_T = table(datas,final_result1, final_result2, final_result3, final_result4);
-final_T.Properties.VariableNames = [{'Data', 'MAD', 'MinMax', 'MADD', 'DMinMax'}];
+final_T = table(datas, indice, final_result1, final_result2, final_result3, final_result4);
+final_T.Properties.VariableNames = [{'Data', 'Ibovespa', 'MAD', 'MinMax', 'MADD', 'DMinMax'}];
 
 ftable_name = ('C:\Users\lilea\Documents\TCC\Matlab\Prog_Linear\tabelas');
 table_name = sprintf('%s-%s_%s-%s.csv', initial_date, final_date, test_initial_date, test_final_date);
