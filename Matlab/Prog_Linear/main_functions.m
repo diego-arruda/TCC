809eb3301fc83d226e2199341c711cfd7a4e236d
Diego <diego.arruda99@gmail.com>
2022-08-27 09:26:02 -0300

    Add met_comp folder

--- Matlab/Prog_Linear/main_functions.m
+++ Matlab/Prog_Linear/main_functions.m
@@ -48,38 +48,38 @@ final_result4 = get_method_result( test_fund_file_names, row, datas, w4 );
 
 % Plot
 
-figure
-plot(final_result1, 'o-');
-hold on;
-plot(final_result2, 'o-');
-hold on;
-plot(final_result3, 'o-');
-hold on;
-plot(final_result4, 'o-');
-hold on;
-plot(indice, 'ko-');
-grid on;
-set(gca, 'XTick', 1:length(datas), 'XTickLabel', datas);
-legend('Rastreamento MAD', 'Rastreamento MinMax', 'Rastreamento MADD', 'Rastreamento DMinMax', 'Ibovespa');
-
-if isequal(initial_date, test_initial_date) && isequal(final_date, test_final_date)
-    titulo = sprintf('Otimizacao e validacao com dados de %s a %s', initial_date, final_date);
-else
-    titulo = sprintf('Otimizacao com dados de %s a %s e validacao com dados de %s a %s', initial_date, final_date, test_initial_date, test_final_date);
-end;
-
-title(titulo)
-
-fname = ('C:\Users\lilea\Documents\TCC\Matlab\Prog_Linear\graficos');
-fig_name = sprintf('%s-%s_%s-%s.png', initial_date, final_date, test_initial_date, test_final_date);
-
-set(gcf, 'PaperUnits', 'centimeters');
-set(gcf, 'PaperPosition', [0 0 30 15]);
-saveas(gca, fullfile(fname, fig_name));
-
-final_T = table(datas, indice, final_result1, final_result2, final_result3, final_result4);
-final_T.Properties.VariableNames = [{'Data', 'Ibovespa', 'MAD', 'MinMax', 'MADD', 'DMinMax'}];
-
-ftable_name = ('C:\Users\lilea\Documents\TCC\Matlab\Prog_Linear\tabelas');
-table_name = sprintf('%s-%s_%s-%s.csv', initial_date, final_date, test_initial_date, test_final_date);
-writetable(final_T,fullfile(ftable_name, table_name));
+% figure
+% plot(final_result1, 'o-');
+% hold on;
+% plot(final_result2, 'o-');
+% hold on;
+% plot(final_result3, 'o-');
+% hold on;
+% plot(final_result4, 'o-');
+% hold on;
+% plot(indice, 'ko-');
+% grid on;
+% set(gca, 'XTick', 1:length(datas), 'XTickLabel', datas);
+% legend('Rastreamento MAD', 'Rastreamento MinMax', 'Rastreamento MADD', 'Rastreamento DMinMax', 'Ibovespa');
+% 
+% if isequal(initial_date, test_initial_date) && isequal(final_date, test_final_date)
+%     titulo = sprintf('Otimizacao e validacao com dados de %s a %s', initial_date, final_date);
+% else
+%     titulo = sprintf('Otimizacao com dados de %s a %s e validacao com dados de %s a %s', initial_date, final_date, test_initial_date, test_final_date);
+% end;
+% 
+% title(titulo)
+% 
+% fname = ('C:\Users\lilea\Documents\TCC\Matlab\Prog_Linear\graficos');
+% fig_name = sprintf('%s-%s_%s-%s.png', initial_date, final_date, test_initial_date, test_final_date);
+% 
+% set(gcf, 'PaperUnits', 'centimeters');
+% set(gcf, 'PaperPosition', [0 0 30 15]);
+% saveas(gca, fullfile(fname, fig_name));
+% 
+% final_T = table(datas, indice, final_result1, final_result2, final_result3, final_result4);
+% final_T.Properties.VariableNames = [{'Data', 'Ibovespa', 'MAD', 'MinMax', 'MADD', 'DMinMax'}];
+% 
+% ftable_name = ('C:\Users\lilea\Documents\TCC\Matlab\Prog_Linear\tabelas');
+% table_name = sprintf('%s-%s_%s-%s.csv', initial_date, final_date, test_initial_date, test_final_date);
+% writetable(final_T,fullfile(ftable_name, table_name));
---
+++ Matlab/met_comp/min_err_quad.m
@@ -0,0 +1,29 @@
+function [ w, z_otimo ] = min_err_quad( y, Gamma, row, n_fund )
+
+    H = 2*Gamma'*Gamma;
+    
+
+    var = 2*row+n_fund;
+
+    I = eye(row);
+    A = [];
+    b = [];
+
+    e = ones(n_fund,1);
+    et = ones(row,1);
+
+    z = zeros(1,row);
+    z1 = zeros(1,n_fund);
+
+    f = [et' et' z1];
+
+    Aeq = [I -I Gamma;
+           z z e'];
+    beq = [y; 1];
+    lb = zeros(var,1);
+
+    [x, z_otimo] = linprog(f,A,b,Aeq,beq,lb);
+    w = x(end-(n_fund-1):end,1);
+
+end
+
