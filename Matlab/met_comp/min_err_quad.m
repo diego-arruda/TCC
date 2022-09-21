69fb695819ba8e5ab823c693dda771c064cd66dd
bruna-maia <bruna.maia@usp.br>
2022-09-21 17:45:35 -0300

    versão testada do script de minimos quadrados

--- Matlab/met_comp/min_err_quad.m
+++ Matlab/met_comp/min_err_quad.m
@@ -1,29 +1,28 @@
 function [ w, z_otimo ] = min_err_quad( y, Gamma, row, n_fund )
 
+% Input:
+% Gamma = matriz com linha - ano, coluna - rentabilidade
+% y = vetor com linha - ano, rentabilidade benchmark
+% row = número de meses
+% n_fund = quantidade de ações
+% z_otimo: valor da função objetivo
+
     H = 2*Gamma'*Gamma;
     
+   %f = -2*y'*Gamma; nosso f anterior
+    f = -2*Gamma'*y;
+ 
+    I = eye(n_fund); 
+    A = [-I];
+    b = zeros(n_fund,1);
 
-    var = 2*row+n_fund;
-
-    I = eye(row);
-    A = [];
-    b = [];
-
-    e = ones(n_fund,1);
-    et = ones(row,1);
-
-    z = zeros(1,row);
-    z1 = zeros(1,n_fund);
-
-    f = [et' et' z1];
-
-    Aeq = [I -I Gamma;
-           z z e'];
-    beq = [y; 1];
-    lb = zeros(var,1);
-
-    [x, z_otimo] = linprog(f,A,b,Aeq,beq,lb);
+    Aeq = ones(1,n_fund); %Restrição e*w = 1 sendo e = Aeq
+    
+    beq = 1;
+   
+    [x, z] = quadprog(H,f,A,b,Aeq,beq);
     w = x(end-(n_fund-1):end,1);
+    z_otimo = z + y'*y;
 
 end
 
